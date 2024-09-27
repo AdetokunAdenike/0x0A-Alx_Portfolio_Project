1b1d157344dcd16d048ed5d0ff5e409b6a8c013d
S-Man129 <adebayosulayman129@gmail.com>
2024-09-27 12:45:14 +0100

    Updates

---
+++ src/js/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    question: "Choose the correct spelling from the options",
+    options: ["Hypopotamus", "Hipopotamus", "Hippopotamus", "Hyppopotamus"],
+    answer: "Hippopotamus",
+  },
+  {
+    question: "Choose the correct spelling from the options",
+    options: ["Elefant", "Helephant", "Elephant", "Elephat"],
+    answer: "Elephant",
+  },
+  {
+    question: "Choose the correct spelling from the options",
+    options: [
+      "Dioxyribonuclaic",
+      "Dioxyribonuclareic",
+      "Deoxyribonuclareic",
+      "Deoxyribonucleic",
+    ],
+    answer: "Deoxyribonucleic",
+  },
+  {
+    question: "Choose the correct spelling from the options",
+    options: ["Flabagasted", "Flerbagasted", "Flabbergersted", "Flabbergasted"],
+    answer: "Flabbergasted",
+  },
+  {
+    question: "Choose the correct spelling from the options",
+    options: ["Doubtfounded", "Dubfounded", "Dumbfounded", "Dumfounded"],
+    answer: "Dumbfounded",
+  },
+];
